3b1838ff4eca009bb1cfbec9da21d9b114f5f748
驰 张 <zliming@outlook.com>
2020-06-18 22:47:21 +0800

    Init

---
+++ GetRich/GetRich/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GetRich.ViewModels.Base
+{
+    public static class ViewModelLocator
+    {
+        public static IContainer Container;
+
+        public static readonly BindableProperty AutoWireViewModelProperty =
+            BindableProperty.CreateAttached("AutoWireViewModel", typeof(bool), typeof(ViewModelLocator), default(bool), propertyChanged: OnAutoWireViewModelChanged);
+
+        public static bool GetAutoWireViewModel(BindableObject bindable)
+        {
+            return (bool)bindable.GetValue(ViewModelLocator.AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(BindableObject bindable, bool value)
+        {
+            bindable.SetValue(ViewModelLocator.AutoWireViewModelProperty, value);
+        }
+
+        static ViewModelLocator()
+        {
+            var builder = new ContainerBuilder();
+
+            Container = builder.Build();
+        }
+
+        private static void OnAutoWireViewModelChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = bindable as Element;
+            if (view == null)
+            {
+                return;
+            }
+
+            var viewType = view.GetType();
+            var viewName = viewType.FullName.Replace(".Views.", ".ViewModels.");
+            var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}Model, {1}", viewName, viewAssemblyName);
+
+            var viewModelType = Type.GetType(viewModelName);
+            if (viewModelType == null)
+            {
+                return;
+            }
+            var viewModel = Container.Resolve(viewModelType);
+            view.BindingContext = viewModel;
+        }
+    }
+}
---
+++ GetRich/GetRich/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GetRich.Models;
+
+namespace GetRich.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
